948e19dd740ba0398a3bfd16da4d428821655d20
wiktoriacg <wiktoria.cioch.gradzik@gmail.com>
2025-02-06 18:57:58 -0600

    Updeted code

--- script.js
+++ script.js
@@ -14,14 +14,18 @@ document.addEventListener("DOMContentLoaded", function () {
         "Oh no, the beast started summoning wyverns!",
         "The beast began to breathe fire. We can say goodbye to the forest",
         "The dragon's claws turned out to be more dangerous than you expected!",
-        "One of your companions ran away, arguing that you weren't paying him enough."
+        "One of your companions ran away, arguing that you weren't paying him enough.",
+        "You're out of arrows. Your companion went for more. Or maybe he just ran away?",
+        "Lightning begins to flash around you. Could this be Night Fury?"
     ];
     const goodMessages = [
         "Good bow shot! The dragon cannot fly for a while",
         "Your companion turned out to be a magician. The dragon lost his confidence",
         "Perfect attack! Even the dragon is surprised",
         "The dagger you bought years ago was finally useful!",
-        "One of the villagers turned out to be a musician. The dragon has lost its rhythm!"
+        "One of the villagers turned out to be a musician. The dragon has lost its rhythm!",
+        "It turns out that the snake repellent oil the weird merchant gave you also works on dragons! After all, it's a reptile too.",
+        "Suddenly, catapult attacks fired at the dragon. Who keeps the catapult in the basement?"
     ];
     function rollDice() {
         const roll = Math.floor(Math.random() * 6) + 1;
